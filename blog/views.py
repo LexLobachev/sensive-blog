c071caeafa683c9a2847b3fdacb2fe9ec9e526c9
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-03-19 14:41:22 +0400

    use shortcut & related name

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Comment, Post, Tag
 from django.db.models import Count, Prefetch
 
@@ -47,8 +47,8 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.annotate(likes_count=Count('likes')).get(slug=slug)
-    comments = Comment.objects.select_related('author').filter(post=post)
+    post = get_object_or_404(Post.objects.annotate(likes_count=Count('likes')), slug=slug)
+    comments = post.comments.all().select_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -87,7 +87,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5]
 
